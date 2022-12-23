c128b08c4dbf029a64a134aa453cd261669f31d5
Yaroslav <yaros6666@yandex.ru>
2022-12-23 17:28:22 +0300

    start

---
+++ Modul_01/1.3.py
@@ -0,0 +1,11 @@
+# class Car:
+#     pass
+#
+# d = {
+# 'model':"Тойота",
+# 'color':"Розовый",
+# 'number':"П111УУ77"
+# }
+# [setattr(Car, name, value) for name, value in d.items()]
+# print(Car.__dict__['color'])
+
